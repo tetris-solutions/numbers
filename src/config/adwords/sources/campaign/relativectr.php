303af1b715f77bdc6a333ea6293c190337e356a3
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-11 17:58:52 -0300

    relativeCTR cannot be inferred

--- src/config/adwords/sources/campaign/relativectr.php
+++ src/config/adwords/sources/campaign/relativectr.php
@@ -10,13 +10,5 @@ return [
     "parse" => function ($data): int {
         return (int)$data->RelativeCtr;
     },
-    "sum" => function (array $rows): float {
-        return array_reduce(
-            $rows,
-            function (float $carry, \stdClass $row): float {
-                return $carry + $row->relativectr;
-            },
-            0.0
-        );
-    }
+    "sum" => NULL
 ];
