b52d9815a7f5a48c3c26638f1e988d1f633ce708
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-11 16:34:02 -0300

    average CPC

--- src/config/adwords/sources/campaign/averagecpc.php
+++ src/config/adwords/sources/campaign/averagecpc.php
@@ -4,11 +4,24 @@ return [
     "entity" => "Campaign",
     "platform" => "adwords",
     "report" => "CAMPAIGN_PERFORMANCE_REPORT",
+    "inferred_from" => ["clicks", "cost"],
     "fields" => [
         "AverageCpc"
     ],
     "parse" => function ($data): float {
         return (float)$data->AverageCpc;
     },
-    "sum" => NULL
+    "sum" => function (array $rows) {
+        $sumClicks = 0;
+        $sumCost = 0;
+
+        foreach ($rows as $row) {
+            $sumClicks += $row->clicks;
+            $sumCost += $row->cost;
+        }
+
+        return $sumCost !== 0
+            ? $sumClicks / $sumCost
+            : 0;
+    }
 ];
