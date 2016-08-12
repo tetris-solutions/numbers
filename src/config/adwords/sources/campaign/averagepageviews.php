e479ac309dee26195423c2784bf1aa191f54bbb9
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-12 17:10:56 -0300

    averagepageviews

--- src/config/adwords/sources/campaign/averagepageviews.php
+++ src/config/adwords/sources/campaign/averagepageviews.php
@@ -10,13 +10,5 @@ return [
     "parse" => function ($data): int {
         return (int)$data->AveragePageviews;
     },
-    "sum" => function (array $rows): float {
-        return array_reduce(
-            $rows,
-            function (float $carry, \stdClass $row): float {
-                return $carry + $row->averagepageviews;
-            },
-            0.0
-        );
-    }
+    "sum" => NULL
 ];
