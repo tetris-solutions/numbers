62c459537cbbdb9da8fff9e0e646122d06c66c97
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-12 17:02:09 -0300

    averagecost

--- src/config/adwords/sources/campaign/averagecost.php
+++ src/config/adwords/sources/campaign/averagecost.php
@@ -4,11 +4,24 @@ return [
     "entity" => "Campaign",
     "platform" => "adwords",
     "report" => "CAMPAIGN_PERFORMANCE_REPORT",
+    "inferred_from" => ["interactions", "cost"],
     "fields" => [
         "AverageCost"
     ],
     "parse" => function ($data): float {
         return (float)$data->AverageCost;
     },
-    "sum" => NULL
+    "sum" => function (array $rows) {
+        $sumInteractions = 0;
+        $sumCost = 0;
+
+        foreach ($rows as $row) {
+            $sumInteractions += $row->interactions;
+            $sumCost += $row->cost;
+        }
+
+        return $sumInteractions !== 0
+            ? $sumCost / $sumInteractions
+            : 0;
+    }
 ];
