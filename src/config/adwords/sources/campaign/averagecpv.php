0855264823eb38cc1630436e50a2783e684d5786
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-12 16:06:45 -0300

    CPV

--- src/config/adwords/sources/campaign/averagecpv.php
+++ src/config/adwords/sources/campaign/averagecpv.php
@@ -4,19 +4,24 @@ return [
     "entity" => "Campaign",
     "platform" => "adwords",
     "report" => "CAMPAIGN_PERFORMANCE_REPORT",
+    "inferred_from" => ["videoviews", "cost"],
     "fields" => [
         "AverageCpv"
     ],
     "parse" => function ($data): int {
         return (int)$data->AverageCpv;
     },
-    "sum" => function (array $rows): float {
-        return array_reduce(
-            $rows,
-            function (float $carry, \stdClass $row): float {
-                return $carry + $row->averagecpv;
-            },
-            0.0
-        );
+    "sum" => function (array $rows) {
+        $sumVideoViews = 0;
+        $sumCost = 0;
+
+        foreach ($rows as $row) {
+            $sumVideoViews += $row->videoviews;
+            $sumCost += $row->cost;
+        }
+
+        return $sumVideoViews !== 0
+            ? $sumCost / $sumVideoViews
+            : 0;
     }
 ];
