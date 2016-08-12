aad8a24202a91b1d3e7b8d4bde8804f75da5025a
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-12 18:18:40 -0300

    valueperallconversion

--- src/config/adwords/sources/campaign/valueperallconversion.php
+++ src/config/adwords/sources/campaign/valueperallconversion.php
@@ -4,19 +4,24 @@ return [
     "entity" => "Campaign",
     "platform" => "adwords",
     "report" => "CAMPAIGN_PERFORMANCE_REPORT",
+    "inferred_from" => ["allconversionvalue", "allconversions"],
     "fields" => [
         "ValuePerAllConversion"
     ],
     "parse" => function ($data): int {
         return (int)$data->ValuePerAllConversion;
     },
-    "sum" => function (array $rows): float {
-        return array_reduce(
-            $rows,
-            function (float $carry, \stdClass $row): float {
-                return $carry + $row->valueperallconversion;
-            },
-            0.0
-        );
+    "sum" => function (array $rows) {
+        $sumAllConversions = 0;
+        $sumAllConversionsValue = 0;
+
+        foreach ($rows as $row) {
+            $sumAllConversions += $row->conversions;
+            $sumAllConversionsValue += $row->allconversionvalue;
+        }
+
+        return $sumAllConversions !== 0
+            ? $sumAllConversionsValue / $sumAllConversions
+            : 0;
     }
 ];
