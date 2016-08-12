93a6f8ba66523c06dce8743dff59b5fb1f546904
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-12 18:20:16 -0300

    interactiontypes sum = NULL

--- src/config/adwords/sources/campaign/interactiontypes.php
+++ src/config/adwords/sources/campaign/interactiontypes.php
@@ -10,13 +10,5 @@ return [
     "parse" => function ($data): int {
         return (int)$data->InteractionTypes;
     },
-    "sum" => function (array $rows): float {
-        return array_reduce(
-            $rows,
-            function (float $carry, \stdClass $row): float {
-                return $carry + $row->interactiontypes;
-            },
-            0.0
-        );
-    }
+    "sum" => NULL
 ];
