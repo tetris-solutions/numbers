78fa7dd909d9667d5a4eac76fac755f2dc241469
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-12 17:11:06 -0300

    average position

--- src/config/adwords/sources/campaign/averageposition.php
+++ src/config/adwords/sources/campaign/averageposition.php
@@ -10,13 +10,5 @@ return [
     "parse" => function ($data): int {
         return (int)$data->AveragePosition;
     },
-    "sum" => function (array $rows): float {
-        return array_reduce(
-            $rows,
-            function (float $carry, \stdClass $row): float {
-                return $carry + $row->averageposition;
-            },
-            0.0
-        );
-    }
+    "sum" => NULL
 ];
