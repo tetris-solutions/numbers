620b8eb13351bec3f02505f34029d802199cace1
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-12 17:30:28 -0300

    engagementrate

--- src/config/adwords/sources/campaign/engagementrate.php
+++ src/config/adwords/sources/campaign/engagementrate.php
@@ -4,11 +4,24 @@ return [
     "entity" => "Campaign",
     "platform" => "adwords",
     "report" => "CAMPAIGN_PERFORMANCE_REPORT",
+    "inferred_from" => ["engagements", "impressions"],
     "fields" => [
         "EngagementRate"
     ],
     "parse" => function ($data): float {
         return floatval(str_replace('%', '', $data->EngagementRate)) / 100;
     },
-    "sum" => NULL
+    "sum" => function (array $rows) {
+        $sumEngagements = 0;
+        $sumImpressions = 0;
+
+        foreach ($rows as $row) {
+            $sumEngagements += $row->engagements;
+            $sumImpressions += $row->impressions;
+        }
+
+        return $sumImpressions !== 0
+            ? $sumEngagements / $sumImpressions
+            : 0;
+    }
 ];
