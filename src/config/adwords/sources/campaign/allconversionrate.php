8d615387cc57b596eb07ae58f6411e1e808ab371
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-12 17:23:57 -0300

    allconversionrate

--- src/config/adwords/sources/campaign/allconversionrate.php
+++ src/config/adwords/sources/campaign/allconversionrate.php
@@ -4,11 +4,24 @@ return [
     "entity" => "Campaign",
     "platform" => "adwords",
     "report" => "CAMPAIGN_PERFORMANCE_REPORT",
+    "inferred_from" => ["allconversions", "clicks"],
     "fields" => [
         "AllConversionRate"
     ],
     "parse" => function ($data): float {
         return floatval(str_replace('%', '', $data->AllConversionRate)) / 100;
     },
-    "sum" => NULL
+    "sum" => function (array $rows) {
+        $sumAllConversions = 0;
+        $sumClicks = 0;
+
+        foreach ($rows as $row) {
+            $sumAllConversions += $row->allconversions;
+            $sumClicks += $row->clicks;
+        }
+
+        return $sumClicks !== 0
+            ? $sumAllConversions / $sumClicks
+            : 0;
+    }
 ];
