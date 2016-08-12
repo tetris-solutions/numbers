cef2221d3ac7f396ee6727943d6de9d95c678c01
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-12 18:12:32 -0300

    videoviewrate

--- src/config/adwords/sources/campaign/videoviewrate.php
+++ src/config/adwords/sources/campaign/videoviewrate.php
@@ -4,11 +4,24 @@ return [
     "entity" => "Campaign",
     "platform" => "adwords",
     "report" => "CAMPAIGN_PERFORMANCE_REPORT",
+    "inferred_from" => ["videoviews", "impressions"],
     "fields" => [
         "VideoViewRate"
     ],
     "parse" => function ($data): float {
         return floatval(str_replace('%', '', $data->VideoViewRate)) / 100;
     },
-    "sum" => NULL
+    "sum" => function (array $rows) {
+        $sumVideoViews = 0;
+        $sumImpressions = 0;
+
+        foreach ($rows as $row) {
+            $sumVideoViews += $row->videoviews;
+            $sumImpressions += $row->impressions;
+        }
+
+        return $sumImpressions !== 0
+            ? $sumImpressions / $sumImpressions
+            : 0;
+    }
 ];
