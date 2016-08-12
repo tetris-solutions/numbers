4daea4d0816e7488850885138c3081c56a785f9d
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-12 17:56:51 -0300

    offlineinteractionrate

--- src/config/adwords/sources/campaign/offlineinteractionrate.php
+++ src/config/adwords/sources/campaign/offlineinteractionrate.php
@@ -4,19 +4,24 @@ return [
     "entity" => "Campaign",
     "platform" => "adwords",
     "report" => "CAMPAIGN_PERFORMANCE_REPORT",
+    "inferred_from" => ["numofflineimpressions", "numofflineinteractions"],
     "fields" => [
         "OfflineInteractionRate"
     ],
     "parse" => function ($data): int {
         return (int)$data->OfflineInteractionRate;
     },
-    "sum" => function (array $rows): float {
-        return array_reduce(
-            $rows,
-            function (float $carry, \stdClass $row): float {
-                return $carry + $row->offlineinteractionrate;
-            },
-            0.0
-        );
+    "sum" => function (array $rows) {
+        $sumNumofflineinteractions = 0;
+        $sumNumofflineimpressions = 0;
+
+        foreach ($rows as $row) {
+            $sumNumofflineinteractions += $row->numofflineinteractions;
+            $sumNumofflineimpressions += $row->numofflineimpressions;
+        }
+
+        return $sumNumofflineimpressions !== 0
+            ? $sumNumofflineinteractions / $sumNumofflineimpressions
+            : 0;
     }
 ];
