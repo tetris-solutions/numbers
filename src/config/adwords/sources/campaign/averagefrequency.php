6e8988ecb0a469ba79971b2f1018e2ebc0ed4bfc
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-12 17:08:03 -0300

    averagefrequency

--- src/config/adwords/sources/campaign/averagefrequency.php
+++ src/config/adwords/sources/campaign/averagefrequency.php
@@ -4,19 +4,24 @@ return [
     "entity" => "Campaign",
     "platform" => "adwords",
     "report" => "CAMPAIGN_PERFORMANCE_REPORT",
+    "inferred_from" => ["impressions", "impressionreach"],
     "fields" => [
         "AverageFrequency"
     ],
     "parse" => function ($data): int {
         return (int)$data->AverageFrequency;
     },
-    "sum" => function (array $rows): float {
-        return array_reduce(
-            $rows,
-            function (float $carry, \stdClass $row): float {
-                return $carry + $row->averagefrequency;
-            },
-            0.0
-        );
+    "sum" => function (array $rows) {
+        $sumImpressions = 0;
+        $sumImpressionsReach = 0;
+
+        foreach ($rows as $row) {
+            $sumImpressionsReach += $row->impressionreach;
+            $sumImpressions += $row->impressions;
+        }
+
+        return $sumImpressionsReach !== 0
+            ? $sumImpressions / $sumImpressionsReach
+            : 0;
     }
 ];
