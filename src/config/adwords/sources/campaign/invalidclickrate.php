4f9adb0dae27d1d6dca0362d6fbc286ec21bf4cd
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-12 17:13:41 -0300

    invalidclickrate

--- src/config/adwords/sources/campaign/invalidclickrate.php
+++ src/config/adwords/sources/campaign/invalidclickrate.php
@@ -4,11 +4,24 @@ return [
     "entity" => "Campaign",
     "platform" => "adwords",
     "report" => "CAMPAIGN_PERFORMANCE_REPORT",
+    "inferred_from" => ["clicks", "invalidclicks"],
     "fields" => [
         "InvalidClickRate"
     ],
     "parse" => function ($data): float {
         return floatval(str_replace('%', '', $data->InvalidClickRate)) / 100;
     },
-    "sum" => NULL
+    "sum" => function (array $rows) {
+        $sumClicks = 0;
+        $sumInvalidClicks = 0;
+
+        foreach ($rows as $row) {
+            $sumClicks += $row->clicks;
+            $sumInvalidClicks += $row->invalidclicks;
+        }
+
+        return $sumClicks !== 0
+            ? $sumInvalidClicks / $sumClicks
+            : 0;
+    }
 ];
