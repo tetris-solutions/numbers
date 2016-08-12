91f8e23826c32c39e40eae6e0d930c5419f0dab4
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-12 17:39:57 -0300

    interactionrate

--- src/config/adwords/sources/campaign/interactionrate.php
+++ src/config/adwords/sources/campaign/interactionrate.php
@@ -4,11 +4,24 @@ return [
     "entity" => "Campaign",
     "platform" => "adwords",
     "report" => "CAMPAIGN_PERFORMANCE_REPORT",
+    "inferred_from" => ["interactions", "impressions"],
     "fields" => [
         "InteractionRate"
     ],
     "parse" => function ($data): float {
         return floatval(str_replace('%', '', $data->InteractionRate)) / 100;
     },
-    "sum" => NULL
+    "sum" => function (array $rows) {
+        $sumInteractions = 0;
+        $sumImpressions = 0;
+
+        foreach ($rows as $row) {
+            $sumInteractions += $row->interactions;
+            $sumImpressions += $row->impressions;
+        }
+
+        return $sumImpressions !== 0
+            ? $sumInteractions / $sumImpressions
+            : 0;
+    }
 ];
