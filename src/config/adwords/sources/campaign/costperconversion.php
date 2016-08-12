31f0ac791992b9ecd267d57be23681e40594bfa8
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-12 16:56:26 -0300

    costperconversion

--- src/config/adwords/sources/campaign/costperconversion.php
+++ src/config/adwords/sources/campaign/costperconversion.php
@@ -4,11 +4,24 @@ return [
     "entity" => "Campaign",
     "platform" => "adwords",
     "report" => "CAMPAIGN_PERFORMANCE_REPORT",
+    "inferred_from" => ["conversions", "cost"],
     "fields" => [
         "CostPerConversion"
     ],
     "parse" => function ($data): float {
         return (float)$data->CostPerConversion;
     },
-    "sum" => NULL
+    "sum" => function (array $rows) {
+        $sumConversions = 0;
+        $sumCost = 0;
+
+        foreach ($rows as $row) {
+            $sumConversions += $row->conversions;
+            $sumCost += $row->cost;
+        }
+
+        return $sumConversions !== 0
+            ? $sumCost / $sumConversions
+            : 0;
+    }
 ];
