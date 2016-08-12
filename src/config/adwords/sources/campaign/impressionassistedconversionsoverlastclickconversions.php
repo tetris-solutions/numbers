0f47e6288f3ffe235422d8c19d84eabf235d59cd
Pablo Lemos de Assis <pablo@cabanacriacao.com>
2016-08-12 16:15:12 -0300

    ImpressionAssistedConversionsOverLastClickConversions sum = NULL

--- src/config/adwords/sources/campaign/impressionassistedconversionsoverlastclickconversions.php
+++ src/config/adwords/sources/campaign/impressionassistedconversionsoverlastclickconversions.php
@@ -10,13 +10,6 @@ return [
     "parse" => function ($data): int {
         return (int)$data->ImpressionAssistedConversionsOverLastClickConversions;
     },
-    "sum" => function (array $rows): float {
-        return array_reduce(
-            $rows,
-            function (float $carry, \stdClass $row): float {
-                return $carry + $row->impressionassistedconversionsoverlastclickconversions;
-            },
-            0.0
-        );
+    "sum" => NULL
     }
 ];
