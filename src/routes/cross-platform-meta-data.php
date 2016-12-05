710fc05156638b6c3d25f36b47ee486d68227ee8
Christian Amaral <darthcas@gmail.com>
2016-12-05 09:46:43 -0200

    changes routes

--- src/routes/cross-platform-meta-data.php
+++ src/routes/cross-platform-meta-data.php
@@ -7,60 +7,62 @@ use Slim\Http\Response;
 
 global $app;
 
-$app->get('/x/meta',
-    function (Request $req, Response $res, array $params): Response {
-        $entity = $req->getQueryParam('entity');
-        $platforms = uniq(explode(',', $req->getQueryParam('platforms')));
+$crossMetaRouteHandler = secured(function (Request $req, Response $res, array $params): Response {
+    $entity = $req->getQueryParam('entity');
+    $platforms = uniq(explode(',', $req->getQueryParam('platforms')));
 
-        $byPlatform = [];
-        $attributes = [];
-        $inventory = [];
+    $byPlatform = [];
+    $attributes = [];
+    $inventory = [];
 
-        $translate = function (string $platform, array $input) use (&$inventory): array {
-            $output = [];
+    $translate = function (string $platform, array $input) use (&$inventory): array {
+        $output = [];
 
-            foreach ($input as $id => $config) {
-                $replacement = MetaData::getReplacementFor($id, $platform);
-                $alternateId = $replacement['id'];
+        foreach ($input as $id => $config) {
+            $replacement = MetaData::getReplacementFor($id, $platform);
+            $alternateId = $replacement['id'];
 
-                $config['id'] = $alternateId;
-                $output[$alternateId] = $config;
+            $config['id'] = $alternateId;
+            $output[$alternateId] = $config;
 
-                if (empty($inventory[$alternateId])) {
-                    $inventory[$alternateId] = [$platform];
-                } else {
-                    $inventory[$alternateId][] = $platform;
-                }
-            }
-
-            return $output;
-        };
-
-        foreach ($platforms as $platform) {
-            $byPlatform[$platform] = $translate($platform,
-                MetaData::listAttributes($platform, $entity));
-        }
-
-        foreach ($inventory as $attributeId => $platformsAttributeAppearsIn) {
-            if (count($platformsAttributeAppearsIn) >= count($platforms)) {
-                $config = [];
-
-                foreach ($platformsAttributeAppearsIn as $platform) {
-                    $config = array_merge($config, $byPlatform[$platform][$attributeId]);
-                }
-
-                $config['requires_id'] = false;
-                $attributes[$attributeId] = $config;
+            if (empty($inventory[$alternateId])) {
+                $inventory[$alternateId] = [$platform];
             } else {
-                $platform = $platformsAttributeAppearsIn[0];
-                $config = $byPlatform[$platform][$attributeId];
-
-                $config['id'] = "{$platform}:{$config['id']}";
-                $config['requires_id'] = true;
-
-                $attributes[$config['id']] = $config;
+                $inventory[$alternateId][] = $platform;
             }
         }
 
-        return $res->withJson($attributes);
-    });
+        return $output;
+    };
+
+    foreach ($platforms as $platform) {
+        $byPlatform[$platform] = $translate($platform,
+            MetaData::listAttributes($platform, $entity));
+    }
+
+    foreach ($inventory as $attributeId => $platformsAttributeAppearsIn) {
+        if (count($platformsAttributeAppearsIn) >= count($platforms)) {
+            $config = [];
+
+            foreach ($platformsAttributeAppearsIn as $platform) {
+                $config = array_merge($config, $byPlatform[$platform][$attributeId]);
+            }
+
+            $config['requires_id'] = false;
+            $attributes[$attributeId] = $config;
+        } else {
+            $platform = $platformsAttributeAppearsIn[0];
+            $config = $byPlatform[$platform][$attributeId];
+
+            $config['id'] = "{$platform}:{$config['id']}";
+            $config['requires_id'] = true;
+
+            $attributes[$config['id']] = $config;
+        }
+    }
+
+    return $res->withJson($attributes);
+});
+
+$app->get('/x/meta', $crossMetaRouteHandler);
+$app->get('/x/meta-data', $crossMetaRouteHandler);
--- src/routes/meta-data.php
+++ src/routes/meta-data.php
@@ -6,10 +6,16 @@ use Slim\Http\Response;
 
 global $app;
 
-$app->get('/meta',
-    secured(function (Request $request, Response $response, array $params) {
-        $entity = $request->getQueryParam('entity');
-        $platform = $request->getQueryParam('platform');
+$metaDataRouteHandler = secured(function (Request $request, Response $response, array $params) {
+    $entity = $request->getQueryParam('entity');
+    $platform = $request->getQueryParam('platform');
+    $attributes = MetaData::listAttributes($platform, $entity);
 
-        return $response->withJson(MetaData::listAttributes($platform, $entity));
-    }));
+    return $response->withJson(isset($params['attribute'])
+        ? $attributes[$params['attribute']]
+        : $attributes);
+});
+
+$app->get('/meta', $metaDataRouteHandler);
+$app->get('/meta-data', $metaDataRouteHandler);
+$app->get('/meta-data/{attribute}', $metaDataRouteHandler);
