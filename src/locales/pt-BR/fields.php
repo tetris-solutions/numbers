145b82249e30909ed104f7dfc624a7cfc0c31e2a
Christian Amaral <darthcas@gmail.com>
2017-05-01 16:07:01 -0300

    localization

--- src/locales/pt-BR/fields.php
+++ src/locales/pt-BR/fields.php
@@ -503,7 +503,7 @@ return [
         'messenger.reply' => 'Message Replys',
         'mobile_app_install' => 'Instalação de Aplicativos Móveis',
         'month' => 'Mês',
-        'month_of_year' => 'Month Of Year',
+        'month_of_year' => 'Mês do ano',
         'new_mobile' => 'Celulares Recêm Comprados',
         'newsfeed_avg_position' => 'Posição Média da Newsfeed',
         'newsfeed_clicks' => 'Cliques na Newsfeed',
