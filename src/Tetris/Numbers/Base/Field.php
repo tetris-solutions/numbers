6f4afed3cbffeca1a1e5cbddf91d36712a287ab2
Christian Amaral <darthcas@gmail.com>
2017-08-03 11:06:58 -0300

    creates include_source env flag; deep path get

--- src/Tetris/Numbers/Base/Field.php
+++ src/Tetris/Numbers/Base/Field.php
@@ -39,9 +39,41 @@ abstract class Field implements ArrayAccess
      */
     public $group;
 
+    private function getPath($pointer, string $path)
+    {
+        $parts = explode('.', $path);
+
+        while (!empty($parts) && (is_object($pointer) || is_array($pointer))) {
+            $property = array_shift($parts);
+
+            if (is_object($pointer) && property_exists($pointer, $property)) {
+                $pointer = $pointer->{$property};
+                continue;
+            }
+
+            if (is_array($pointer)) {
+                if (array_key_exists($property, $pointer)) {
+                    $pointer = $pointer[$property];
+                    continue;
+                }
+
+                $property = is_numeric($property) ? (int)$property : $property;
+
+                if (array_key_exists($property, $pointer)) {
+                    $pointer = $pointer[$property];
+                    continue;
+                }
+            }
+
+            $pointer = NULL;
+        }
+
+        return $pointer;
+    }
+
     protected function getValue($source)
     {
-        return $source->{$this->property} ?? null;
+        return $source->{$this->property} ?? $this->getPath($source, $this->property);
     }
 
     protected function getNumericValue($source): string
